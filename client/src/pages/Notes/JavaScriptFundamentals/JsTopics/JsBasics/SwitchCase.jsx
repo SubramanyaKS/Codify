e09a59b9724e2da2461a222c77e7e25e4c8c6faf
deepanshu-prajapati01 <deepanshubawaliya@gmail.com>
2025-09-09 14:04:49 +0530

    removed old unused imports

--- client/src/pages/Notes/JavaScriptFundamentals/JsTopics/JsBasics/SwitchCase.jsx
+++ client/src/pages/Notes/JavaScriptFundamentals/JsTopics/JsBasics/SwitchCase.jsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import CodeBlock from '../../components/CodeBlock';
 
 const SwitchCase = () => {
