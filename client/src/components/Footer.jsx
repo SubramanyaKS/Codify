7ee1274d580e5121515e47b04f38c6bff610c922
Roshansuthar2023@gmail.com <roshansuthar2023@gmail.com>
2025-10-01 10:54:28 +0530

    Visitors count is now working

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -15,16 +15,13 @@ import roadmap from "../assets/json/rolebasedRoadmaps.json";
 import { useEffect, useState } from "react";
 import NewsletterSubscribeInput from "./NewsletterSubscribeInput";
 const Footer = () => {
-  const [visitorCount, setVisitorCount] = useState(null);
-
+  const [visitorCount, setVisitorCount] = useState(0);
   useEffect(() => {
     fetch(`${import.meta.env.VITE_SERVER_API}/api/v1/auth/`)
       .then(async res => {
-        
-        const data = await res.json() 
-      console.log("data" , data)
-      } )
-      .then(data => setVisitorCount(data.visitorCount))
+        const data = await res.json();
+        setVisitorCount(data.visitorCount); // Set state here
+      })
       .catch(err => console.error("Error fetching visitors:", err));
   }, []);
   const { theme } = useTheme();
